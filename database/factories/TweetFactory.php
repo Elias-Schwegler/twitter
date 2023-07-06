8c5b4f2f308cf9af6b3718db63a8eded58144b64
elias schwegler <elias.schwegler03@gmail.com>
2023-07-06 13:12:12 +0200

    D1 finished

--- database/factories/TweetFactory.php
+++ database/factories/TweetFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use App\Models\Tweet;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -12,6 +13,7 @@ class TweetFactory extends Factory
     public function definition()
     {
         return [
+            'user_id' => User::factory(),
             'text' => $this->faker->text(160),
             'likes' => $this->faker->numberBetween(0, 20000),
             'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
--- database/migrations/2023_07_06_093101_create_tweets_table.php
+++ database/migrations/2023_07_06_093101_create_tweets_table.php
@@ -1,32 +1,55 @@
 <?php
 
+
+
+
 use Illuminate\Database\Migrations\Migration;
+
 use Illuminate\Database\Schema\Blueprint;
+
 use Illuminate\Support\Facades\Schema;
 
+
+
+
 return new class extends Migration
+
 {
+
     /**
+
      * Run the migrations.
+
      */
-    public function up()
-    {
-        Schema::table('tweets', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('text', 160);
-            $table->unsignedInteger('likes')->default(0);
-            $table->timestamps();
-        });
-    }
+
+     public function up()
+     {
+         if (!Schema::hasTable('tweets')) {
+             Schema::create('tweets', function (Blueprint $table) {
+                 $table->id();
+                 $table->foreignId('user_id')->constrained()->onDelete('cascade');
+                 $table->string('text', 160);
+                 $table->unsignedInteger('likes')->default(0);
+                 $table->timestamps();
+             });
+         }
+     }
+     
 
 
 
     /**
+
      * Reverse the migrations.
+
      */
+
     public function down(): void
+
     {
+
         Schema::dropIfExists('tweets');
+
     }
+
 };
