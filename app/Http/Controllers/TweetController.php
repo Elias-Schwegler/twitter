ba19670fc7d99ba017b79b2805071ffea0331f45
elias schwegler <elias.schwegler03@gmail.com>
2023-07-06 11:39:33 +0200

    first commit

---
+++ app/Http/Controllers/TweetController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tweet;
+
+class TweetController extends Controller
+{
+    public function index()
+    {
+        $tweets = Tweet::all();
+
+        // Tweet-Objekte ändern
+        $tweets = $tweets->map(function ($tweet) {
+            $tweet->user = [
+                "name" => "Franzi Musterfrau"
+            ];
+            return $tweet;
+        });
+
+        return ['data' => $tweets];
+    }
+}
